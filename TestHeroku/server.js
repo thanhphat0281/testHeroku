6f0ba5d92de666480ea2e4baaafaa4e4d354908f
ThanhPhat <thanhphat0281@gmail.com>
2022-01-04 12:17:47 +0700

    update

---
+++ TestHeroku/server.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const favicon = require('serve-favicon');
+const app = express();
+const forceSSL = function () {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+};
+app.use(express.static('./dist/testHeroku'));
+ 
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname,'/dist/testHeroku/index.html'));
+});
+ 
+app.use(forceSSL());
+ 
+app.listen(process.env.PORT || 8080);
